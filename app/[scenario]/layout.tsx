2a6bba08d5b5f3f2f6ad1de2b9f3fed9bd4e4184
Nick Saukin <i@nsauk.in>
2024-04-15 21:12:20 +0200

    Add 'use client' to layout

--- app/[scenario]/layout.tsx
+++ app/[scenario]/layout.tsx
@@ -1,10 +1,12 @@
+'use client'
+
 import { useContext } from 'react'
 import { IntegrationAppProvider } from '@integration-app/react'
 import FlowRunLogProvider from '@/components/flow-run-log-provider'
 
 import { TokenContext } from '@/components/token-provider'
 
-export default function InternalLayout({
+export default function InnerLayout({
   children,
 }: {
   children: React.ReactNode
@@ -12,11 +14,8 @@ export default function InternalLayout({
   const token = useContext(TokenContext) as string
 
   return (
-    <div>
-      <div>this is a line from [scenario]/layout.tsx</div>
     <IntegrationAppProvider token={token}>
       <FlowRunLogProvider>{children}</FlowRunLogProvider>
     </IntegrationAppProvider>
-    </div>
   )
 }
